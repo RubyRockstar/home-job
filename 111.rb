b41023285c651881b4452554572037c509fe247b
murad <murad1110488@gmail.com>
2020-01-22 21:40:48 +0300

    исправение

--- 111.rb
+++ 111.rb
@@ -1,18 +1,15 @@
-puts 'a = '
+  puts 'a = '
 a = gets.chomp.to_f
-puts 'b = '
+  puts 'b = '
 b = gets.chomp.to_f
-puts 'c = '
+  puts 'c = '
 c = gets.chomp.to_f
- 
-d  = Math.sqrt b * b - 4 * a * c
- 
+d =  (b * b - 4 * a * c).to_i
 if (d == 0)
-   puts 'x = '+ (- b / 2 / a).to_s
+  puts 'x = '+ (- b / 2 / a).to_s
 elsif (d > 0)
-      puts 'x1 = ' + (( - b - d) / 2 / a ).to_s
-      puts 'x2 = ' + ((- b + d) / 2 / a ).to_s
-   else (d < 0)
-	puts "корней нет"
+  puts 'x1 = ' + (( - b - Math.sqrt(d)) / 2 / a ).to_s
+  puts 'x2 = ' + ((- b + Math.sqrt(d)) / 2 / a ).to_s
+else (d < 0)
+  puts "корней нет"
 end
-
--- Pifagor.rb
+++ Pifagor.rb
@@ -1,14 +1,14 @@
-puts "сторона a"
+  puts "сторона a"
 a = gets.chomp.to_i
-puts " сторона b"
+  puts " сторона b"
 b = gets.chomp.to_i
- puts "сторона c"
+  puts "сторона c"
 c = gets.chomp. to_i
- if a == b + c || b == c + a || c == a + b 
-puts " этот треугольник прямоугольный "
- end
- if a == b && a == c
-	puts "этот треугольник равносторонний"
-  else a == b && a < c
-   puts "этот треугольник равнобедренный"
-  end
+if a == b + c || b == c + a || c == a + b 
+  puts " этот треугольник прямоугольный "
+end
+if a == b && a == c
+  puts "этот треугольник равносторонний"
+else a == b && a < c
+  puts "этот треугольник равнобедренный"
+end
