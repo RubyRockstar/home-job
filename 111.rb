4614b80eeeeb96dbac9e74a44eccb8e564c3fcea
murad <murad1110488@gmail.com>
2020-01-23 12:27:11 +0300

    1

--- 111.rb
+++ 111.rb
@@ -6,10 +6,11 @@ puts 'c = '
 c = gets.chomp.to_f
 d =  (b * b - 4 * a * c).to_i
 if (d == 0)
-puts 'x = '+ (- b / 2 / a).to_s
+  puts 'x = '+ (- b / 2 / a).to_s
 elsif (d > 0)
-puts 'x1 = ' + (( - b - Math.sqrt(d)) / 2 / a ).to_s
-puts 'x2 = ' + ((- b + Math.sqrt(d)) / 2 / a ).to_s
+sqrt_d = Math.sqrt(d)
+  puts "x1 = #{(- b - sqrt_d) / 2 / a}"
+  puts "x2 = #{(- b + sqrt_d) / 2 / a}"
 else (d < 0)
-puts "корней нет"
+  puts "корней нет"
 end
--- Weight.rb
+++ Weight.rb
@@ -2,4 +2,4 @@ puts "введите Ваше имя"
 name = gets.chomp
 puts "введите Ваш рост"
 weight = gets.chomp
-puts "#{name}, твой идеальный вес #{(110 - weight. to_i) * 1.15}"  	
+puts "#{name}, твой идеальный вес #{(110 - weight. to_i) * 1.15}"
